b85dd23cfc4ba8e151dd44bf3412518c2c44f8fa
jpgerber1986 <jordangerber@gmail.com>
2025-07-22 13:49:07 -0600

    See Changelog v1.7.1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ require_once __DIR__ . "/_auth.php";
 require_once __DIR__ . "/_console.php";
 require_once __DIR__ . "/_dev.php";
 require_once __DIR__ . "/_open.php";
+require_once __DIR__ . "/_guest.php";
