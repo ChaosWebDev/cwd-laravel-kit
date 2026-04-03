597ade9139b1579314b4100ae85f2311382b90b8
Jordan Gerber <jordangerber@gmail.com>
2026-04-03 08:50:45 -0600

    Fixed Routes files and added api.php route file.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Livewire\Dashboard;
 use Illuminate\Support\Facades\Route;
 
-require_once __DIR__ . "/_console.php";
 require_once __DIR__ . "/_dev.php";
 
-Route::get('/',Dashboard::class)->name('dashboard');
+Route::livewire('/', 'pages::dashboard')->name('dashboard');
